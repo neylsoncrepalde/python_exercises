d15cffc48796b6667c67c04f97d2e4c691fb9bc5
neylsoncrepalde <neylsoncrepalde@gmail.com>
2018-01-19 15:22:42 +0100

    finaliza classes - testes prontos - falta teste carrinho

--- Produtos/produto.py
+++ Produtos/produto.py
@@ -1,7 +1,13 @@
 # Exercício Programação Orientada a Objetos
 
 class Produto:
-    def __init__(self, nome, codigo, preco)
-    self.nome = nome
-    self.codigo = codigo
-    self.preco = preco
+    def __init__(self, nome, codigo, preco):
+        self.nome = nome
+        self.codigo = codigo
+        self.preco = preco
+        
+    def produtosIguais(self, produto2):
+        if self.codigo == produto2.codigo:
+            return True
+        else:
+            return False
--- Produtos/produtocomtamanho.py
+++ Produtos/produtocomtamanho.py
@@ -7,3 +7,9 @@ class ProdutoComTamanho(Produto):
         Produto.__init__(self, nome, codigo, preco)
         self.tamanho = tamanho
         
+    def produtoComTamanhoIguais(self, produto2):
+        if self.produtosIguais(produto2):
+            if self.tamanho == produto2.tamanho:
+                return True
+        else:
+            return False
