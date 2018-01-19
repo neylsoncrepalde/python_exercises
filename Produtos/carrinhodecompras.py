09eae37022963c43b73305c49c642ef893ff3f1d
neylsoncrepalde <neylsoncrepalde@gmail.com>
2018-01-19 15:20:23 +0100

    correcoes

--- Produtos/carrinhodecompras.py
+++ Produtos/carrinhodecompras.py
@@ -2,19 +2,43 @@
 # Falta implementar produtos com tamanhos diferentes
 # serão considerados diferentes
 
-from produtocomtamanho import ProdutoComTamanho
-
 class CarrinhoDeCompras:
     def __init__(self):
         self.items = []
         self.quantidades = {}
 
     def adicionaProduto(self, produto, quantidade):
-        self.items.append(produto)
+        if produto not in self.items:
+            self.items.append(produto)
+        
         if produto.nome not in self.quantidades:
-            self.quantidades[produto.nome] = 0
-            self.quantidades[produto.nome] += quantidade
+            self.quantidades[produto.nome] = {"quantidade": 0,
+                                              "preco": produto.preco}
+            self.quantidades[produto.nome]["quantidade"] += quantidade
         else:
-            self.quantidades[produto.nome] += quantidade
-            
-    # Continuar implementando removeProduto
+            jatem = [item.produtoComTamanhoIguais(produto) for \
+                     item in self.items]
+            if True in jatem:
+                self.quantidades[produto.nome]["quantidade"] += quantidade
+            else:
+                self.quantidades[produto.nome] = {"quantidade": quantidade,
+                                                  "preco": produto.preco}
+        
+    def removeProduto(self, produto, quantidade):
+        if produto not in self.items:
+            raise ValueError("Não há um item deste produto no carrinho.")
+        else:
+            jatem = [item.produtoComTamanhoIguais(produto) for \
+                     item in self.items]
+            if True in jatem:
+                self.quantidades[produto.nome]["quantidade"] -= quantidade
+    
+    # Calcula o valor da compra
+    def calculaValorCompra(self):
+        valorTotal = 0
+        for item in self.quantidades.keys:
+            valorParcial = item["quantidade"] * item["preco"]
+            valorTotal += valorParcial
+        
+    
+    
